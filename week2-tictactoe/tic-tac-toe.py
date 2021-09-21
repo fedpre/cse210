afcb1f13cdedaab4d2195d3b0a4fe45f861e3aea
fedpre <fed.pregnolato@gmail.com>
2021-09-21 10:16:42 +0200

    Need to finilize win_draw_checker()

--- week2-tictactoe/tic-tac-toe.py
+++ week2-tictactoe/tic-tac-toe.py
@@ -14,31 +14,46 @@ def main():
     incorrect_number = True
     player_x = 'X'
     player_o = 'O'
+    is_finished = False
+    number_chosen = []
 
     while game_finished != True:
         x_selection = int(input(f"x's turn to choose a square (1-{max_val}): "))
         while incorrect_number != False:
             if x_selection < 1 or x_selection > max_val:
                 incorrect_number = True
-                x_selection = int(input(f"Incorrect value. Please choose a value between 1 and {max_val}): "))
+                x_selection = int(input(f"Incorrect value. Please choose a value between 1 and {max_val}: "))
+            elif x_selection in number_chosen:
+                incorrect_number = True
+                x_selection = int(input(f"Value already chosen. Please choose a value between 1 and {max_val} not already chosen: "))
             else:
                 incorrect_number = False
-
+        number_chosen.append(x_selection)
         updated_grid = change_element(x_selection, grid, player_x)
         draw_grid(updated_grid, n_digits)
-
+        is_finished, winner = win_checker(updated_grid, player_x, player_o)
+        if is_finished:
+            print(f"Congratulations player {winner}! You won the game")
+            break
+        
+        incorrect_number = True
         o_selection = int(input(f"o's turn to choose a square (1-{max_val}): "))
         while incorrect_number != False:
-            if x_selection < 1 or x_selection > max_val:
+            if o_selection < 1 or o_selection > max_val:
                 incorrect_number = True
-                x_selection = int(input(f"Incorrect value. Please choose a value between 1 and {max_val}): "))
+                o_selection = int(input(f"Incorrect value. Please choose a value between 1 and {max_val}: "))
+            elif o_selection in number_chosen:
+                incorrect_number = True
+                o_selection = int(input(f"Value already chosen. Please choose a value between 1 and {max_val} not already chosen: "))
             else:
                 incorrect_number = False
-
+        number_chosen.append(o_selection)
         updated_grid = change_element(o_selection, grid, player_o)
         draw_grid(updated_grid, n_digits) 
-        game_finished = win_checker(updated_grid)
-        break
+        is_finished, winner = win_draw_checker(updated_grid, player_x, player_o)
+        if is_finished:
+            print(f"Congratulations player {winner}! You won the game")
+            break
             
 
 
@@ -59,8 +74,8 @@ def draw_grid(grid_array, n_digits):
     for row in grid_array:
         for element in row:
             print(f"{element:{n_digits}}", end=' ')
-            if element == row[-1]:
-                continue
+            # if element == row[-1]:
+            #     continue
             print('|', end = ' ')
         if row == grid_array[-1]:
             print()
@@ -76,7 +91,7 @@ def draw_grid(grid_array, n_digits):
         else:
             print('---', end = '')
             for _ in range(len(row)):
-                if _ == range(len(row))[-1]:
+                if _ == range(len(row))[-1] and n_digits >= 2:
                     continue
                 print('+----', end = '')   
             print()
@@ -90,8 +105,27 @@ def change_element(number, grid_array, player):
                 row[element_index] = player
                 return grid_array
 
-def win_checker(grid_array):
-    pass
+def win_draw_checker(grid_array, player_x, player_o):
+    player_x_counter = 0
+    player_o_counter = 0
+    for row in grid_array:
+        for element in row:
+            if element == player_x:
+                player_x_counter += 1
+            elif element == player_o:
+                player_o_counter += 1
+        if player_x_counter == len(row):
+            return True, player_x
+        elif player_o_counter == len(row):
+            return True, player_o
+        else:
+            player_o_counter = 0
+            player_x_counter = 0
+    ## Check for vertical win, diagonal win, or draw ## 
+
+
+    #####################################
+    return False, ''
 
 
 if __name__ == '__main__':
